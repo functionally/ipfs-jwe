a4b1de4cf0e382d43fa433fd246d80bd670cde50
Brian W Bush <code@functionally.io>
2024-07-20 17:44:59 -0600

    Implemented CGI client

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+  "bufio"
   "compress/gzip"
   "context"
   "errors"
+  "fmt"
   "io"
   "io/ioutil"
+  "os"
   "github.com/ipfs/go-cid"
   "github.com/ipfs/boxo/files"
   "github.com/ipfs/boxo/path"
@@ -64,6 +67,18 @@ func getCid(ctx context.Context, key jwk.Key) (*cid.Cid, error) {
   return &cid, nil
 }
 
+func getMime(ctx context.Context, key jwk.Key) (*string, error) {
+  meta, metaErr := key.AsMap(ctx)
+  if metaErr != nil {
+    return nil, metaErr
+  }
+  mime, mimeExists := meta["mime"].(string)
+  if !mimeExists {
+    return nil, errors.New("Metadata key `mime` not found.")
+  }
+  return &mime, nil
+}
+
 func fetchCid(ctx context.Context, node *rpc.HttpApi, cid cid.Cid) (io.Reader, error) {
   path := path.FromCid(cid)
   file, ipfsErr := node.Unixfs().Get(ctx, path)
@@ -72,7 +87,7 @@ func fetchCid(ctx context.Context, node *rpc.HttpApi, cid cid.Cid) (io.Reader, e
   }
   reader, fileExists := file.(files.File)
   if !fileExists {
-      return nil, errors.New("Not a `files.File`.")
+    return nil, errors.New("Not a `files.File`.")
   }
   return reader, nil
 }
@@ -89,6 +104,32 @@ func fetchCompressedEncrypted(ctx context.Context, node *rpc.HttpApi, key jwk.Ke
   return decryptCompressed(key, jweReader)
 }
 
+func geminiError(code int, err error) []byte {
+  status := fmt.Sprintf("%d %s\r\n", code, err.Error())
+  return []byte(status)
+}
+
+func handleGemini(ctx context.Context, node *rpc.HttpApi, keys map[string]jwk.Key, kid string) []byte {
+
+  key, keyErr := keys[kid]
+  if !keyErr {
+    return geminiError(51, errors.New("Key `" + kid + "` not found."))
+  }
+
+  mime, mimeErr := getMime(ctx, key)
+  if mimeErr != nil {
+    return geminiError(42, mimeErr)
+  }
+
+  plainBuf, fetchErr := fetchCompressedEncrypted(ctx, node, key)
+  if fetchErr != nil {
+    return geminiError(42, fetchErr)
+  }
+
+  status := fmt.Sprintf("%d %s\r\n\r\n", 20, *mime)
+  return append([]byte(status), plainBuf...)
+}
+
 func connectIpfs(api string) (*rpc.HttpApi, error) {
   addr, addrErr := ma.NewMultiaddr(api)
     if addrErr != nil {
@@ -97,27 +138,58 @@ func connectIpfs(api string) (*rpc.HttpApi, error) {
   return rpc.NewApi(addr)
 }
 
-func main () {
+func readKeys(keysFile string) (map[string]jwk.Key, error) {
 
-  var jwkFile = "./tmp.jwk"
-  var binFile = ".tmp/bin"
-  var api = "/ip4/192.168.0.9/tcp/5001"
+  handle, openErr := os.Open(keysFile)
+  if openErr != nil {
+    return nil, openErr
+  }
+  defer handle.Close()
+
+  keys := make(map[string]jwk.Key)
+  scanner := bufio.NewScanner(handle)
+  for scanner.Scan() {
+    line := scanner.Bytes()
+    key, keyErr := jwk.ParseKey(line)
+    if keyErr != nil {
+      return nil, keyErr
+    }
+    keys[key.KeyID()] = key
+  }
+
+  return keys, nil
+}
+
+func cgiGemini(keysFile string, api string) {
+
+  keys, keysErr := readKeys("./tmp.keys")
+  if keysErr != nil {
+    os.Stdout.Write(geminiError(42, errors.New("Failed reading keys file.")))
+    return
+  }
 
   ctx := context.Background()
   node, connectErr := connectIpfs(api)
   if connectErr != nil {
-      panic(connectErr)
+    os.Stdout.Write(geminiError(42, errors.New("Filed to connect to IPFS.")))
+    return
   }
 
-  key, keyErr := readKey(jwkFile)
-  if keyErr != nil {
-    panic(keyErr)
+  kid := os.Getenv("PATH_INFO")
+  if kid == "" {
+    os.Stdout.Write(geminiError(59, errors.New("Missing key.")))
+    return
+  } else {
+    response := handleGemini(ctx, node, keys, kid)
+    _, writeErr := os.Stdout.Write(response)
+    if writeErr != nil {
+      os.Stdout.Write(geminiError(42, writeErr))
+      return
+    }
   }
 
-  plainBuf, fetchErr := fetchCompressedEncrypted(ctx, node, key)
-  if fetchErr != nil {
-    panic(fetchErr)
-  }
+}
 
-  ioutil.WriteFile(binFile, plainBuf, 0640)
+func main() {
+  cgiGemini("./tmp/keys", "/ip4/192.168.0.9/tcp/5001")
 }
